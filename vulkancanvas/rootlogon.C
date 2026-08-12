012eb5e8fec38d5b5ccd325280cb8812b3eb340a
Sergey Linev <S.Linev@gsi.de>
2026-08-12 08:34:00 +0200

    remove first vulkan

--- vulkancanvas/rootlogon.C
+++
@@ -1,9 +0,0 @@
-{
-   gSystem->AddDynamicPath(".");
-
-   gEnv->SetValue("Gui.Factory", "vulkan");
-
-   gPluginMgr->AddHandler("TGuiFactory", "vulkan",
-                          "ROOT::Experimental::TVulkanGuiFactory",
-                          "libROOTVulkanCanvas", "TVulkanGuiFactory()");
-}
